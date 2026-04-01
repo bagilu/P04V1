60206d864ddbd991055aba7a43e9569ad096b978
C. M. Lin <bagilu@gmail.com>
2026-04-01 10:13:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.APP_CONFIG = {
   SITE_URL: 'https://bagilu.github.io/P04V1/',
   EMAIL_DOMAIN: '@gms.tcu.edu.tw',
   STORAGE_KEY_ACCOUNT: 'p04_smile_account',
-  SUPABASE_URL: 'https://YOUR-PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY',
+  SUPABASE_URL: 'https://mfljkyvdadxlrbxlboce.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs',
   TABLE_SMILE_EVENTS: 'tblp04smileevents',
   TABLE_MAIL_LOG: 'tblp04maillog'
 };
